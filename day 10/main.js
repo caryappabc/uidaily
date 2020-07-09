648d92693cedcc02bfaa5a2834b7c75f9bc43882
caryappabc <37845613+caryappabc@users.noreply.github.com>
2020-07-09 11:20:56 +0530

    Add files via upload

---
+++ day 10/main.js
@@ -0,0 +1,12 @@
+
+var f_s = getElementById("font_size").value;
+var f_f = getElementById("font").value;
+function set(){
+    document.getElementById("s").style.display="block";
+}
+ function change(){
+    document.getElementsByTagName("p").style.font-family(f_f);
+    document.getElementsByTagName("p").style.font-size(f_s);
+}
+
+ var name = "Caryappa B C";
